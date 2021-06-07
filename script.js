ad9aa281a28c4ababdb68f3166c254c525b5db31
Francesco Bellomi <bellomifrancesco@live.it>
2021-06-07 16:47:14 +0200

    Aggiunto javascript

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 let i = 0;
-let txt = "Benvenuto , Mi chiamo Francesco Bellomi e sono un Junior Web Developer! Scorri in basso per sapere di più su di me.";
+let txt = "Mi chiamo Francesco Bellomi e sono un Junior Web Developer! Scorri in basso per sapere di più su di me.";
 let speed = 70;
 
-function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("benvenuto").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+setTimeout(() => {
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("benvenuto").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
     }
+    typeWriter();
+}, 4000);
+
+let project = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < project.length; i++) {
+    $("#b" + project[i]).click(function() {
+        $("#p" + project[i]).toggleClass("col-sm-4");
+        $(this).toggleClass("fa-search-plus").toggleClass("fa-search-minus");
+    });
 }
 
-typeWriter();
+$(".work").addClass("tx-black")
+
+$('html, body').css({
+    overflow: "hidden",
+    height: "100%"
+});
+
+$(window).on("load", function() {
+    $(".loader-wrapper").fadeOut(1500);
+    $('html, body').css({
+        overflow: "auto",
+        height: "auto"
+    });
+})
